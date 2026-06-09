ef6c6a36e9c5f24f948789a31e3e90ed9a958e3e
Kotopeska <dima05morozov@mail.ru>
2026-06-09 18:58:35 +0300

    add new record: Artist

--- src/ru/rsreu/morozov/datalayer/data/Artist.java
+++ src/ru/rsreu/morozov/datalayer/data/Artist.java
@@ -1,4 +1,5 @@
 package ru.rsreu.morozov.datalayer.data;
 
-public record Artist() {
+public record Artist(String name, String country, String labelName) {
+    public static final Artist DEFAULT = new Artist("NULL", "NULL", "NULL");
 }
